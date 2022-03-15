fe67d97371a66b6f3c03a86c0968740484735749
Yulia <bannyboo729@gmail.com>
2022-03-15 20:35:58 +0300

    wa2

---
+++ wa2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 22;
+int b = 3;
+int c = 9;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
