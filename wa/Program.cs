556c244701737b758fbc900733a8ae7a6f60d888
Yulia <bannyboo729@gmail.com>
2022-03-15 20:16:52 +0300

    wa1

--- wa/Program.cs
+++ wa/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+﻿int a = -9;
+int b = -3;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+int max = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
